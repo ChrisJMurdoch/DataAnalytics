8c9e50f7e695e1d8d9f00998f46a0680e06b53ee
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2022-01-25 15:35:48 +0000

    Added comments

--- exercises/Exercise 12/exercise.js
+++ exercises/Exercise 12/exercise.js
@@ -9,30 +9,42 @@ var svg = d3.select("body")
 // Load each line of the scene CSV onto the page
 d3.csv("https://raw.githubusercontent.com/ChrisJMurdoch/DataAnalytics/master/data/scene.csv", function(data) {
     switch (data.shape) {
+
+        // Create rectangle
         case "rect":
             var shape = svg.append("rect")
                 .attr("x", data.x).attr("y", data.y)
                 .attr("width", data.width).attr("height", data.height);
             break;
+
+        // Create circle
         case "circle":
             var shape = svg.append("circle")
                 .attr("cx", data.x).attr("cy", data.y)
                 .attr("r", data.width);
             break;
+            
+        // Create ellipse
         case "ellipse":
             var shape = svg.append("ellipse")
                 .attr("cx", data.x).attr("cy", data.y)
                 .attr("rx", data.width).attr("ry", data.height);
             break;
+            
+        // Create text
         case "text":
             var shape = svg.append("text")
                 .attr("x", data.x).attr("y", data.y)
                 .text(data.text);
             break;
+        
+        // Display error
         default:
             svg.append("p").text("Shape-parsing error.");
             return data;
     }
+
+    // Set colours (same for all shapes)
     shape.attr("stroke", data.stroke).attr("fill", data.fill);
 
     return data;
