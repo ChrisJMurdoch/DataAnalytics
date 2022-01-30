3d1361f74ecfbaa2d62ac4713c2c19b8819a9bc8
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2022-01-30 13:05:27 +0000

    P9 data

--- exercises/Exercise 17/exercise.js
+++ exercises/Exercise 17/exercise.js
@@ -1,43 +1,70 @@
 
-const url = "https://raw.githubusercontent.com/ChrisJMurdoch/DataAnalytics/master/data/part_nine_data.csv";
+/**
+ * Take in the URL of a CSV file and display selected column as a bar chart
+ * @param  {String} csvUrl URl of the CSV file to be loaded
+ * @param  {String} columnName Name of the column to be displayed
+ * @param  {Function} callback Called when async function finishes
+*/
+function displayCsvAsBar(csvUrl, columnName, callback) {
 
-// Read in data from CSV
-d3.csv(url).then(function(data) {
-        
-    const data = [50, 400, 300, 900, 250, 1000]
-    const width = 500;
-    const barHeight = 20;
-    const margin = 1;
+    // Read in data from CSV
+    d3.csv(csvUrl, function(data) {
 
-    // Create scale
-    var scale = d3.scaleLinear()
-        .domain([d3.min(data), d3.max(data)])
-        .range([50, 500]);
+        // Use data from the first column if no column is specified
+        return (typeof(columnName)==='undefined') ? data[Object.keys(data)[0]] : data[columnName];
 
-    // Add SVG
-    var svg = d3.select("body")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", barHeight * data.length);
+    // Use the data once loaded
+    }).then(function(data) {
 
-    // Create SVG groups
-    var g = svg.selectAll("g")
-        .data(data)
-        .enter()
-        .append("g")
-        .attr("transform", (d, i) => `translate(0,${i*barHeight})`);
+        // Set display dimensions
+        const width = 500;
+        const barHeight = 20;
+        const margin = 1;
 
-    // Create coloured bar
-    g.append("rect")
-        .attr("width", (d) => scale(d) )
-        .attr('fill', (d) => d<100 ? 'green' : d>500 ? 'red' : 'yellow' ) // Switch colour based on unscaled value
-        .attr("height", barHeight - margin)
+        // Create scale from data
+        var scale = d3.scaleLinear()
+            .domain([d3.min(data), d3.max(data)])
+            .range([50, 500]);
 
-    // Display data values
-    g.append("text")
-        .attr("x", (d) => scale(d) )
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .style('text-anchor', 'end')
-        .text( (d) => d );
-})
+        // Add SVG
+        var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", barHeight * data.length);
+
+        // Create SVG groups
+        var g = svg.selectAll("g")
+            .data(data)
+            .enter()
+            .append("g")
+            .attr("transform", (d, i) => `translate(0,${i*barHeight})`);
+
+        // Create coloured bar
+        g.append("rect")
+            .attr("width", (d) => scale(d) )
+            .attr('fill', (d) => d<100 ? 'green' : d>500 ? 'red' : 'yellow' ) // Switch colour based on unscaled value
+            .attr("height", barHeight - margin)
+
+        // Display data values
+        g.append("text")
+            .attr("x", (d) => scale(d) )
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .style('text-anchor', 'end')
+            .text( (d) => d );
+
+        // Call callback
+        try {
+            callback();
+        } catch (e) {}
+    })
+}
+
+// Call function the first time with callback
+d3.select("body").append("h3").text("Part 9 data");
+displayCsvAsBar("https://raw.githubusercontent.com/ChrisJMurdoch/DataAnalytics/master/data/part_nine_data_a.csv", "value", function() {
+
+    // Call function the second time with no callback
+    d3.select("body").append("h3").text("Part 6 scene data");
+    displayCsvAsBar("https://raw.githubusercontent.com/ChrisJMurdoch/DataAnalytics/master/data/part_nine_data_b.csv", "value");
+});
